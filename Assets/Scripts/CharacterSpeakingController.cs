31aeb9f0ecfdcdbab56a446728d82c74b6160001
harrivin <harrivinn@gmail.com>
2016-04-21 11:11:37 -0400

    scenes added
    added scenes

--- Assets/Scripts/CharacterSpeakingController.cs
+++ Assets/Scripts/CharacterSpeakingController.cs
@@ -3,82 +3,31 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class CharacterSpeakingController : MonoBehaviour {
-	public GameObject sarah, jay;
-	public AudioClip[] sarahSpeeches, jaySpeeches;
-	AudioSource sarahAudioSource, jayAudioSource;
-	string[] sarahAnimationNames, jayAnimationNames;
-	Animation sarahAnimation, jayAnimation;
-	public FaceFXControllerScript sarahFFX, jayFFX;
-
-	HeadLookController sarahHeadLook, jayHeadLook;
-
-	GameObject sarahHead, jayHead, playerHead;
-
-	public AudioClip decisionQuestion;
-	public AudioClip sarahChoice;
-	public AudioClip jayChoice;
-
-	public bool decisionMade = false;
-	public bool atDecisionPoint = false;
-	bool decisionAsked = false;
-	public GameObject currentVH;
-
-	int sarahIndex = 0;
-	int jayIndex = 0;
-
-	public AnimationClip[] talkingAnimations;
-
+	public GameObject vh1;
+	public AudioClip[] vh1Speeches;
+	AudioSource vh1AudioSource;
+	string[] vh1AnimationNames;
+	Animation vh1Animation;
+	public FaceFXControllerScript vh1FFX;
 
 	// Use this for initialization
 	void Start () {
-		sarahAnimation = sarah.GetComponent<Animation> ();
-		sarahFFX = sarah.GetComponent<FaceFXControllerScript> ();
-		sarahAudioSource = sarah.GetComponent<AudioSource> ();
-
-		jayAnimation = jay.GetComponent<Animation> ();
-		jayFFX = jay.GetComponent<FaceFXControllerScript> ();
-		jayAudioSource = jay.GetComponent<AudioSource> ();
-
+		vh1Animation = vh1.GetComponent<Animation> ();
+		vh1FFX = vh1.GetComponent<FaceFXControllerScript> ();
+		vh1AudioSource = vh1.GetComponent<AudioSource> ();
 		setActualAnimations ();
-		currentVH = sarah;
-
-		sarahHead = GameObject.FindGameObjectWithTag ("sarahHead");
-		jayHead = GameObject.FindGameObjectWithTag ("jayHead");
-		playerHead = GameObject.FindGameObjectWithTag ("playerHead");
-		sarahHeadLook = sarah.gameObject.GetComponent<HeadLookController> ();
-		jayHeadLook = jay.gameObject.GetComponent<HeadLookController> ();
+		Invoke ("playAnimation", 2);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (canTakeTurn ()) {
-			StartCoroutine ("playSpeech");
-		}
-		if (atDecisionPoint & canTakeTurn() & !decisionAsked) {
-			if (currentVH.name == "sarah") {
-				jayAudioSource.clip = decisionQuestion;
-				jayAudioSource.Play ();
-				jayHeadLook.target = playerHead.transform.position;
-				jayFFX.PlayAnim (jayAnimationNames [jayIndex], decisionQuestion);
-				
-			} else {
-				sarahAudioSource.clip = decisionQuestion;
-				sarahAudioSource.Play ();
-				sarahHeadLook.target = playerHead.transform.position;
-				sarahFFX.PlayAnim (sarahAnimationNames [sarahIndex], decisionQuestion);
-			}
-
-			decisionAsked = true;
-			
-		}
-
-		
+	
 	}
 
 	void setActualAnimations(){
-		Debug.Log (sarahAnimation.GetClipCount ());
+		Debug.Log (vh1Animation.GetClipCount ());
 		List<string> temp = new List<string> ();
-		foreach(AnimationState state in sarahAnimation)
+		foreach(AnimationState state in vh1Animation)
 		{
 			if (state.name.Contains ("Default")) {
 				//Debug.Log (state.name);
@@ -86,71 +35,14 @@ public class CharacterSpeakingController : MonoBehaviour {
 			}
 		}
 
-		sarahAnimationNames = temp.ToArray ();
-
-		temp = new List<string> ();
-		foreach(AnimationState state in jayAnimation)
-		{
-			if (state.name.Contains ("Default")) {
-				//Debug.Log (state.name);
-				temp.Add (state.name);
-			}
-		}
-
-		jayAnimationNames = temp.ToArray ();
+		vh1AnimationNames = temp.ToArray ();
 	}
-
-	IEnumerator playSpeech(){
-		float rand = Random.value;
-		if (!atDecisionPoint & !decisionMade) {
-			Debug.Log ("playing speech of " + currentVH.name);
-			if (currentVH.name == "sarah" && sarahIndex < sarahSpeeches.Length) {
-				sarahAudioSource.clip = sarahSpeeches [sarahIndex];
-				sarahAudioSource.Play ();
-				currentVH = jay;
-				sarahFFX.PlayAnim(sarahAnimationNames[sarahIndex], sarahSpeeches[sarahIndex]);
-				sarahIndex++;
-				if (rand > 0.5f) {
-					sarahHeadLook.target = jayHead.transform.position;
-				} else {
-					sarahHeadLook.target = playerHead.transform.position;
-				}
-				jayHeadLook.target = sarahHead.transform.position;
-				yield return new WaitForSeconds (sarahAudioSource.clip.length);
-
-			} else if (currentVH.name == "jay" && jayIndex < jaySpeeches.Length) {
-				jayAudioSource.clip = jaySpeeches [jayIndex];
-				jayAudioSource.Play ();
-				currentVH = sarah;
-				jayFFX.PlayAnim(jayAnimationNames[jayIndex], jaySpeeches[jayIndex]);
-				jayIndex++;
-
-				if (rand > 0.5f) {
-					jayHeadLook.target = sarahHead.transform.position;
-				} else {
-					jayHeadLook.target = playerHead.transform.position;
-				}
-				sarahHeadLook.target = jayHead.transform.position;
-				yield return new WaitForSeconds (jayAudioSource.clip.length);
-
-			} else {
-				atDecisionPoint = true;
-			}
-		}
-
-
-	}
-
-	bool canTakeTurn(){
-		return !sarahAudioSource.isPlaying && !jayAudioSource.isPlaying;
-	}
-
 
 	void playAnimation(){
-		//Debug.Log (sarahAnimationNames [0]);
-		sarahAnimation.Play(sarahAnimationNames[0]);
-		sarahAudioSource.clip = sarahSpeeches[0];
-		sarahAudioSource.Play ();
-		sarahFFX.PlayAnim(sarahAnimationNames[0], sarahSpeeches[0]);
+		Debug.Log (vh1AnimationNames [0]);
+		//vh1Animation.Play(vh1AnimationNames[0]);
+		vh1AudioSource.clip = vh1Speeches[0];
+		vh1AudioSource.Play ();
+		vh1FFX.PlayAnim(vh1AnimationNames[0], vh1Speeches[0]);
 	}
 }
---
+++ Assets/Scripts/ControlProbe.js
@@ -0,0 +1,36 @@
+﻿#pragma strict
+
+var plane : GameObject;
+
+var character : GameObject;
+
+var offset : float;
+
+var directionFaced : Direction1;
+
+
+
+
+function Update () {
+if (directionFaced == Direction1.X){
+offset = (plane.transform.position.x - character.transform.position.x);
+transform.position.x= plane.transform.position.x + offset;
+transform.position.y= character.transform.position.y;
+transform.position.z= character.transform.position.z;
+}
+if (directionFaced == Direction1.Y){
+offset = (plane.transform.position.y - character.transform.position.y);
+transform.position.x= character.transform.position.x;
+transform.position.y= plane.transform.position.y + offset;
+transform.position.z= character.transform.position.z;
+}
+if (directionFaced == Direction1.Z){
+offset = (plane.transform.position.z - character.transform.position.z);
+transform.position.x= character.transform.position.x;
+transform.position.y= character.transform.position.y;
+transform.position.z= plane.transform.position.z + offset;
+}
+}
+public enum Direction1{
+X,Y,Z
+}
