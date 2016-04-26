1ec16b90c647154d2911f42f8219e771e634f6d5
Stevie Carnell <scarnell@ufl.edu>
2016-04-26 17:16:07 -0400

    fixed ui on park scene, added audio to park

---
+++ Assets/SarahController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SarahController : MonoBehaviour {
+
+	AudioSource sarahAS;
+	public AudioClip[] sarahSpeeches;
+
+	// Use this for initialization
+	void Start () {
+		sarahAS = this.gameObject.GetComponent<AudioSource> ();
+		if (DecisionManager.predictedFuture == "good") {
+			sarahAS.clip = sarahSpeeches [0];
+		} else {
+			sarahAS.clip = sarahSpeeches [1];
+		}
+
+		sarahAS.Play ();
+	
+	}
+	
+
+}
